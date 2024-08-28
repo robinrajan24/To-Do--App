4187e2c5d8f1e7146e45d9c9bfb534ab44faac59
robinrajan24 <robinrajan24@gmail.com>
2024-08-28 12:24:25 +0530

    added the edit and delete todo function

--- script.js
+++ script.js
@@ -1,80 +1,173 @@
-const todoValue=document.getElementById('todoText')
-const todoAlert=document.getElementById('Alert')
-const listItems=document.getElementById('list-items')
-const addUpdate=document.getElementById('AddupdateClick')
+const todoValue = document.getElementById("todoText");
+const todoAlert = document.getElementById("Alert");
+const listItems = document.getElementById("list-items");
+const addUpdate = document.getElementById("AddupdateClick");
 
-let todo=JSON.parse(localStorage.getItem('todo-list'));
-if(!todo){
-    todo=[]
+let todo = JSON.parse(localStorage.getItem("todo-list"));
+if (!todo) {
+  todo = [];
 }
 
+// Creating task
+function CreateTodoItems() {
+  if (todoValue.value === "") {
+    todoAlert.innerHTML = "Please enter your todo text";
+    todoValue.focus();
+  } else {
+    let isPresent = false;
+    // if the array value and entered value are same then show msg already added
+    todo.forEach((ele) => {
+      if (ele.item == todoValue.value) {
+        isPresent - true;
+      }
+    });
 
-function CreateTodoItems(){
-    if(todoValue.value===''){
-        todoAlert.innerHTML='Please enter your todo text'
-        todoValue.focus()
+    if (isPresent) {
+      setAlertMessage("This item already present in the list");
     }
-    else{
-      
-        let isPresent=false;
-         // if the array value and entered value are same then show msg already added
-        todo.forEach((ele)=>{
-            if(ele.item==todoValue.value){
-                isPresent-true
-            }
-        })
-
-        if(isPresent){
-            setAlertMessage(
-                "This item already present in the list"
-            )
-
-        }
-// adding to list 
-        let li=document.createElement("li")
-        const todoItems=`<div title="Hit Double Click and Complete"ondblcclick="CompleteToDoItems(this)">
+    // adding to list
+    let li = document.createElement("li");
+    const todoItems = `<div title="Hit Double Click and Complete"ondblcclick="CompleteToDoItems(this)">
         ${todoValue.value}
         </div>
         <div>
-        <img class="edit todo-controls" occlick="UpdateToDoItems(this)" src="/images/pencil.png">
+        <img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="/images/pencil.png">
 
-         <img class="delete todo-controls" occlick="DeleteToDoItems(this)" src="/images/trash-bin.png">
+         <img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="/images/trash-bin.png">
         
         </div>
 `;
-         li.innerHTML=todoItems;
-         listItems.appendChild(li)
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
 
-
-         if(!todo){
-            todo=[]
-        }
-
-        let itemList={item:todoValue.value,status:false}
-        todo.push(itemList)
-        console.log(todo,"todo")
-        setLocalStorage()
-
-
-
-        
-
+    if (!todo) {
+      todo = [];
     }
 
-    todoValue.value='';
-        setAlertMessage("Todo item Created Successfully")
+    let itemList = { item: todoValue.value, status: false };
+    todo.push(itemList);
+    console.log(todo, "todo");
+    setLocalStorage();
+  }
 
-   
-
+  todoValue.value = "";
+  setAlertMessage("Todo item Created Successfully");
 }
 
-function setLocalStorage(){
-    localStorage.setItem("todo-list",JSON.stringify(todo))
+function setLocalStorage() {
+  localStorage.setItem("todo-list", JSON.stringify(todo));
 }
 
+// Reading Task
 
+function ReadToDoItems() {
+  todo.forEach((ele) => {
+    console.log(ele, "read todo");
+    let li = document.createElement("li");
+    let style = "";
 
+    if (ele.status) {
+      style = "style='text-decoration:line-through'";
+    }
 
+    const todoItems = `
+        <div ${style} title="Hit Double Click and Complete"ondblclick="CompleteToDoItems(this)">
+        ${ele.item}
+        ${
+          style === ""
+            ? ""
+            : '<img class="todo-controls" src="/images/tick.png">'
+        }
+        
+        </div>
+        
+        <div>
 
+         ${
+           style === ""
+             ? '<img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="/images/pencil.png">'
+             : ""
+         }
 
-  
+        <img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="/images/trash-bin.png">
+
+    
+        
+       
+        
+        </div>`;
+
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+  });
+}
+
+ReadToDoItems();
+// edit todo
+function UpdateToDoItems(e) {
+  console.log(e, "ele");
+  if (
+    e.parentElement.parentElement.querySelector("div").style.textDecoration ===
+    ""
+  ) {
+    todoValue.value =
+      e.parentElement.parentElement.querySelector("div").innerText;
+    updateText = e.parentElement.parentElement.querySelector("div");
+    addUpdate.setAttribute("onclick", "UpdateOnSelectedItems()");
+    addUpdate.setAttribute("src", "/images/loading.gif");
+    todoValue.focus();
+  }
+}
+
+// edit and update todo
+function UpdateOnSelectedItems() {
+  let isPresent = false;
+  todo.forEach((ele) => {
+    if (ele.item == todoValue.value) {
+      isPresent = true;
+    }
+  });
+
+  if (isPresent) {
+    console.log("value already added");
+    setAlertMessage("This item is already present in the list");
+    return;
+  }
+  todo.forEach((ele) => {
+    if (ele.item == updateText.innerHTML.trim()) {
+      ele.item = todoValue.value;
+    }
+  });
+
+  setLocalStorage();
+  updateText.innerHTML = todoValue.value;
+  addUpdate.setAttribute("onclick", "CreateTodoItems()");
+  addUpdate.setAttribute("src", "/images/plus.png");
+  todoValue.value = "";
+  setAlertMessage("Todo item Updated Successfully");
+}
+
+// delete todo
+
+function DeleteToDoItems(e) {
+  console.log(e);
+  let deleteValue =
+    e.parentElement.parentElement.querySelector("div").innerText;
+
+  if (confirm(`Are you sure. Due you want to delete this ${deleteValue}!`)) {
+    e.parentElement.parentElement.setAttribute("class", "deleted-item");
+    todoValue.focus();
+
+    todo.forEach((ele) => {
+      if (ele.item == deleteValue.trim()) {
+        todo.splice(ele, 1);
+      }
+    });
+
+    setTimeout(() => {
+      e.parentElement.parentElement.remove();
+    }, 1000);
+
+    setLocalStorage();
+  }
+}
